402b49fad6464aae46e2ad12b7548bce3b7a9042
Rogério Pereira <rogerio@groupsofter.com.br>
2015-11-20 15:50:58 -0200

    7.8 Estilizando template principal

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,11 +1,30 @@
 <html>
     <head>
         <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/custom.css" rel="stylesheet">
         <title>Controle de Estoque</title>
     </head>
     <body>
         <div class='container'>
+            <nav class='navbar navbar-default'>
+                <div class='container-fluid'>
+                    <div class='navbar-header'>
+                        <a class="navbar-brand" href='/produtos'>
+                            Estoque Laravel
+                        </a>
+                    </div>
+
+                    <ul class='nav navbar-nav navbar-right'>
+                        <li><a href='/produtos'>Listagem</a></li>
+                    </ul>
+                </div>
+            </nav>
+
             @yield('conteudo');
+
+            <footer class='footer'>
+                <p>&copy; Livro de Laravel da Casa do Codigo</p>
+            </footer>
         </div>
     </body>
 </html>
