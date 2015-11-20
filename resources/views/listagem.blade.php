37510f332fbf170449f5a87ab87c97da59c351c4
Rogério Pereira <rogerio@groupsofter.com.br>
2015-11-20 15:39:38 -0200

    7.6 Marcando itens em falta no estoque

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -9,21 +9,25 @@
         <h1>Listagem de Produtos</h1>
 
         <table class='table table-striped table-bordered table-hover'>
-
-        @foreach ($produtos as $produto)
-            <tr>
-                <td>{{$produto->nome}}</td>
-                <td>{{$produto->valor}}</td>
-                <td>{{$produto->descricao}}</td>
-                <td>{{$produto->quantidade}}</td>
-                <td>
-                    <a href="produtos/mostra/{{$produto->id}}">
-                        Visualizar
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-
+            @foreach ($produtos as $produto)
+                <tr class='{{ $produto->quantidade <= 1 ? 'danger' : ''}}'>
+                    <td>{{$produto->nome}}</td>
+                    <td>{{$produto->valor}}</td>
+                    <td>{{$produto->descricao}}</td>
+                    <td>{{$produto->quantidade}}</td>
+                    <td>
+                        <a href="produtos/mostra/{{$produto->id}}">
+                            Visualizar
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
         </table>
+
+        <h4>
+            <span class='label label-danger pull-right'>
+                Um ou menos itens no estoque
+            </span>
+        </h4>
     @endif
 @stop
