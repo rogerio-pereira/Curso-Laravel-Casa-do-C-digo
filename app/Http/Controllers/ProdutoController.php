8fc1d02f5ac585badd61c84b6920317bf6bb14bd
Rogério Pereira <rogerio@groupsofter.com.br>
2015-11-20 12:26:28 -0200

    3.4 Configuração e conexão com MySQL

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,11 +1,29 @@
 <?php
     namespace estoque\Http\Controllers;
 
+    use Illuminate\Support\Facades\DB;
+
     class ProdutoController extends Controller
     {
         public function lista()
         {
-            return '<h1>Listagem de Produtos com Laravel</h1>';
+            $html = '<h1>Listagem de Produtos com Laravel</h1>';
+            $html .= '<ul>';
+
+            $produtos = DB::select('SELECT * FROM produtos');
+
+            foreach ($produtos as $produto) {
+                $html .= '
+                            <li>
+                                Nome: '.$produto->nome.'
+                                Descriçao: '.$produto->descricao.'
+                            </li>
+                        ';
+            }
+
+            $html .= '</ul>';
+
+            return $html;
         }
     }
 ?>
