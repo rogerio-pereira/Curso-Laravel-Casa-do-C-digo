2352b50f9a49bfe5222760db5f74f013616acca7
Rogério Pereira <rogerio@groupsofter.com.br>
2015-11-21 19:29:37 -0200

    11.4 Exibindo erros de validaçao

--- resources/views/produto/formulario.blade.php
+++ resources/views/produto/formulario.blade.php
@@ -3,6 +3,16 @@
 @section('conteudo')
     <h1>Novo Produto</h1>
 
+    @if(count($errors) > 0)
+        <div class='alert alert-danger'>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="/produtos/adiciona" method="POST">
         <input type='hidden' name='_token' value='{{{csrf_token()}}}' />
 
