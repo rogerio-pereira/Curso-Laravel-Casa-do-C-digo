dc29b6341b018226d3e4623eea1fdf7e4a2c38cb
Rogério Pereira <rogerio@groupsofter.com.br>
2015-11-21 19:42:36 -0200

    11.7 Mantendo os valores nos imputs

--- resources/views/produto/formulario.blade.php
+++ resources/views/produto/formulario.blade.php
@@ -18,22 +18,22 @@
 
         <div class='form-group'>
             <label>Nome</label>
-            <input name='nome' class='form-control'/>
+            <input name='nome' class='form-control' value='{{old('nome')}}'/>
         </div>
 
         <div class='form-group'>
             <label>Descricao</label>
-            <input name='descricao' class='form-control'/>
+            <input name='descricao' class='form-control' value='{{old('descricao')}}'/>
         </div>
 
         <div class='form-group'>
             <label>Valor</label>
-            <input name='valor' class='form-control'/>
+            <input name='valor' class='form-control' value='{{old('valor')}}'/>
         </div>
 
         <div class='form-group'>
             <label>Quantidade</label>
-            <input type='number' name='quantidade' class='form-control'/>
+            <input type='number' name='quantidade' class='form-control' value='{{old('quantidade')}}'/>
         </div>
 
         <button type='submit' class='btn btn-primary btn-block'>Submit</button>
